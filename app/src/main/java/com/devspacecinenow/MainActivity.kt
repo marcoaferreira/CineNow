7d94c6ebbb2ef88d062f9219ae63cc04fa2a9095
marco <marcoaferreira@gmail.com>
2024-10-02 09:23:14 -0300

    [minor] created retrofit client

--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -11,36 +11,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.devspacecinenow.ui.theme.CineNowTheme
+import retrofit2.create
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CineNowTheme {
+
+                val apiService = RetrofitClient.retrofitInstance.create(ApiService::class.java)
+                apiService.getNowPlayingMovies()
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CineNowTheme {
-        Greeting("Android")
     }
 }
